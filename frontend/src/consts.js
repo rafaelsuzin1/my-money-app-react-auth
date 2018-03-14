48603f18952dca0e08aa1e8d159b1737d0ceb737
suzin <rafael.suzin@itagtecnologia.com.br>
2018-03-13 22:32:41 -0300

    Versao inicial

---
+++ frontend/src/consts.js
@@ -0,0 +1,6 @@
+export default {
+  //  API_URL: 'https://cod3r-my-money-app-backend.herokuapp.com/api',
+   // OAPI_URL: 'https://cod3r-my-money-app-backend.herokuapp.com/oapi',
+   API_URL: 'http://localhost:3003/api',
+   OAPI_URL: 'http://localhost:3003/oapi',
+}
